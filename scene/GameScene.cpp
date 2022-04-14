b89b8a29a8f78010df27a964247ee8341fd4799b
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-14 15:17:17 +0900

    マリオの画像の追加 ３Dモデル画像の表示 音声の追加　等

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,11 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() 
+{ 
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +18,40 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	model_ = Model::Create();
+	
+	worldTransform_.Initialize();
+
+	viewProjection_.Initialize();
+
+	soundDateHandle_ = audio_->LoadWave("se_sad03.wav");
+
+	voiceHandle_ = audio_->PlayWave(soundDateHandle_,true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ 
+	XMFLOAT2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//反映
+	sprite_->SetPosition(position);
+	if (input_->TriggerKey(DIK_SPACE)) 
+	{
+		//音声の停止
+		audio_->StopWave(voiceHandle_);
+	}
+	value_++;
+	//値を含んだ文字列
+	std::string strDebug = std::string("value:") + std::to_string(value_);
+	//デバックテキストの表示
+	debugText_->Print(strDebug, 50, 50, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +79,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -56,7 +93,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
