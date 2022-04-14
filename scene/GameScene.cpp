72118f77dea420766347091abf0f71dbfc5edec6
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-14 15:34:31 +0900

    merge

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,10 +6,7 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() 
-{ 
-	delete model_;
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
@@ -17,53 +14,9 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
-	//ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("mario.jpg");
-	//３Dモデルの生成
-	model_ = Model::Create();
-
-	//X、Y、Z方向のスケーリングを設定
-	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	//X、Y、Z軸周りの回転角を設定(単位はラジアン)
-	worldTransform_.rotation_ = {XMConvertToRadians(45.0f), XM_PI / 4.0f, 0.0f}; // XM_PIは180度
-	//// X、Y、Z軸周りの回転角を設定(単位はラジアン)
-	//worldTransform_.rotation_ = {0.0f,XMConvertToRadians(45.0f), 0.0f};
-	//X、Y、Z軸周りの平行移動を設定
-	worldTransform_.translation_ = {10.0f,10.0f,10.0f};
-
-	//☝の３つは同時に機能する
-
-	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
-	//ビュープロジェクションの初期化
-	viewProjection_.Initialize();
-	
 }
 
-void GameScene::Update() 
-{ 
-
-	std::string strDebugTra = std::string("transtion:(") + 
-	std::to_string(translationX) + std::string(",") + 
-	std::to_string(translationY) + std::string(",") + 
-	std::to_string(translationZ) + std::string(")");
-	debugText_->Print(strDebugTra, 50, 50, 1.0f);
-
-	std::string strDebugRota = std::string("rotation:(") + 
-	std::to_string(rotationX) + std::string(",") + 
-	std::to_string(rotationY) + std::string(",") + 
-	std::to_string(rotationZ) + std::string(")");
-	debugText_->Print(strDebugRota, 50, 70, 1.0f);
-
-	std::string strDebugScale = std::string("scale:(") + 
-	std::to_string(scaleX) + std::string(",") + 
-	std::to_string(scaleY) + std::string(",") + 
-	std::to_string(scaleZ) + std::string(")");
-	debugText_->Print(strDebugScale, 50, 90, 1.0f);
-	
-	
-	
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -91,9 +44,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
-	//3Dモデル描画
-	model_->Draw(worldTransform_,viewProjection_,textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -48,30 +48,6 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
-
-	//モデルの表示
-	Model* model_ = nullptr;
-	
-	//ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	//ビュープロジェクション
-	ViewProjection viewProjection_;
-
-	////値を表示したい変数
-	float translationX = 10.0f;
-	float translationY = 10.0f;
-	float translationZ = 10.0f;
-
-	float rotationX = 0.785398f;
-	float rotationY = 0.785398f;
-	float rotationZ = 0.0f;
-
-	float scaleX = 5.0f;
-	float scaleY = 5.0f;
-	float scaleZ = 5.0f;
-
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
