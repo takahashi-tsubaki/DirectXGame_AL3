49527c573a5e910ceb99ace0dc19ce0cbc38b65d
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-14 17:17:10 +0900

    seed

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,6 +1,7 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include <random>
 
 using namespace DirectX;
 
@@ -19,26 +20,47 @@ void GameScene::Initialize() {
 	//３Dモデルの生成
 	model_ = Model::Create();
 
-	// X、Y、Z方向のスケーリングを設定
-	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	// X、Y、Z軸周りの回転角を設定(単位はラジアン)
-	worldTransform_.rotation_ = {XMConvertToRadians(45.0f), XM_PI / 4.0f, 0.0f}; // XM_PIは180度
-	//// X、Y、Z軸周りの回転角を設定(単位はラジアン)
-	// worldTransform_.rotation_ = {0.0f,XMConvertToRadians(45.0f), 0.0f};
-	// X、Y、Z軸周りの平行移動を設定
-	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	//乱数シード生成器
+	std::random_device seed_gen;
+	//メンセンヌ・ツイスター
+	std::mt19937_64 engine(seed_gen());
+	//乱数範囲(回転角用)
+	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
+	//乱数範囲(座標用)
+	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
 
+	for (size_t i = 0;i< _countof(worldTransform_);i++) 
+	{
+		// X、Y、Z方向のスケーリングを設定
+		worldTransform_[i].scale_ = {5.0f, 5.0f, 5.0f};
+		// X、Y、Z軸周りの回転角を設定(単位はラジアン)
+		worldTransform_[i].rotation_ = {
+		  rotDist(engine), rotDist(engine), rotDist(engine)}; // XM_PIは180度
+		// X、Y、Z軸周りの平行移動を設定
+		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
+
+		// X、Y、Z軸周りの回転角を設定(単位はラジアン)
+		// worldTransform_.rotation_ = {0.0f,XMConvertToRadians(45.0f), 0.0f};
+
+		//ワールドトランスフォームの初期化
+		worldTransform_[i].Initialize();
+	}
+
+	
 	//☝の３つは同時に機能する
 
-	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
+	
+	
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
+
+	
+
 }
 
 void GameScene::Update() {
 
-	std::string strDebugTra = std::string("transtion:(") + std::to_string(translationX) +
+	/*std::string strDebugTra = std::string("transtion:(") + std::to_string(translationX) +
 	                          std::string(",") + std::to_string(translationY) + std::string(",") +
 	                          std::to_string(translationZ) + std::string(")");
 	debugText_->Print(strDebugTra, 50, 50, 1.0f);
@@ -51,7 +73,7 @@ void GameScene::Update() {
 	std::string strDebugScale = std::string("scale:(") + std::to_string(scaleX) + std::string(",") +
 	                            std::to_string(scaleY) + std::string(",") + std::to_string(scaleZ) +
 	                            std::string(")");
-	debugText_->Print(strDebugScale, 50, 90, 1.0f);
+	debugText_->Print(strDebugScale, 50, 90, 1.0f);*/
 }
 
 void GameScene::Draw() {
@@ -82,7 +104,12 @@ void GameScene::Draw() {
 	/// </summary>
 
 	// 3Dモデル描画
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	/*model_->Draw(worldTransform_, viewProjection_, textureHandle_);*/
+
+	for (size_t i = 0; i < _countof(worldTransform_); i++) 
+	{
+		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
