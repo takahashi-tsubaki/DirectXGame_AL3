b0da362e630d8e3125e78924ac748f3622d6c100
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-21 15:49:16 +0900

    課題の完成

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -20,34 +20,79 @@ void GameScene::Initialize() {
 	//３Dモデルの生成
 	model_ = Model::Create();
 
-	for (size_t i = 0; i< _countof(worldTransform_); i++) 
-	{
-		for (size_t j = 0; j < _countof(worldTransform_); j++) 
-		{
-			for (size_t k = 0; k < _countof(worldTransform_); k++) 
-			{
-				////X,Y,Z方向のスケーリングを設定
-				// worldTransform_[i].scale_ = {5.0f, 5.0f, 5.0f};
+	//乱数シード生成器
+	std::random_device seed_gen;
+	//メルセンヌ・ツイスター
+	std::mt19937_64 engine(seed_gen());
+	//乱数範囲(回転角用)
 
-				//////X,Y,Z軸回りの回転角を設定
-				////worldTransform_[i].rotation_ = {};
+	//乱数範囲(座標用)
+	std::uniform_real_distribution<float> posDist(-20.0f, 10.0f); //最小値と最大値
 
-				// X,Y,Z軸回りの平行移動を設定
-				worldTransform_[k][j][i].translation_ = {-12.0f + (3.0f * i),12.0f -(3.0f * j), (4.0f * k)};
+	
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
 
-				//ワールドトランスフォームの初期化
-				worldTransform_[k][j][i].Initialize();
-			}
-			
-		}
+	////X,Y,Z軸回りの回転角を設定
+	//worldTransform_.rotation_ = {};
+
+	// X,Y,Z軸回りの平行移動を設定
+	worldTransform_.translation_ = {0.0f ,0.0f,0.0f};
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
 		
-	}
-	//ビュープロジェクションの初期化
-	viewProjection_.Initialize();
+	for (int i = 0;i<_countof(viewProjection_);i++) 
+	{
+		/*eyePosX[i] = posDist(engine);
+		eyePosY[i] = posDist(engine);
+		eyePosZ[i] = posDist(engine);*/
 
+		//
+		/*viewProjection_[i].eye = {eyePosX[i], eyePosY[i], eyePosZ[i]};*/
+		viewProjection_[i].eye = {posDist(engine), posDist(engine), posDist(engine)};
+		//
+		viewProjection_[i].target = {10,0,0};
+
+		// 
+		viewProjection_[i].up = {0.0f,1.0f, 0.0f};
+
+		//ビュープロジェクションの初期化
+		viewProjection_[i].Initialize();
+
+	}
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ 
+	
+	if (input_->TriggerKey(DIK_SPACE)) {
+		if (cameraNum < 2) {
+			cameraNum++;
+		} else {
+			cameraNum = 0;
+		}
+	}
+		
+	for (int i = 0; i < _countof(viewProjection_); i++)
+	{
+		debugText_->SetPos(50, 50 + (i*70));
+		/*debugText_->Printf("camera = %d", cameraNum);*/
+		debugText_->Printf(
+		  "eye:(%f,%f,%f)\n", viewProjection_[i].eye.x, viewProjection_[i].eye.y,
+		  viewProjection_[i].eye.z);
+		debugText_->SetPos(50, 70 + (i * 70));
+		debugText_->Printf(
+		  "target:(%f,%f,%f)\n", viewProjection_[i].target.x, viewProjection_[i].target.y,
+		  viewProjection_[i].target.z);
+		debugText_->SetPos(50, 90 + (i * 70));
+		debugText_->Printf(
+		  "up:(%f,%f,%f)\n", viewProjection_[i].up.x, viewProjection_[i].up.y,
+		  viewProjection_[i].up.z);
+
+	}
+	
+}
 
 void GameScene::Draw() {
 
@@ -77,19 +122,7 @@ void GameScene::Draw() {
 	/// </summary>
 
 	// 3Dモデル描画
-
-	for (size_t i = 0; i < _countof(worldTransform_); i++) 
-	{
-		for (size_t j = 0; j < _countof(worldTransform_); j++) 
-		{
-			for (size_t k = 0; k < _countof(worldTransform_); k++) 
-			{
-				model_->Draw(worldTransform_[k][j][i], viewProjection_, textureHandle_);
-			}
-		}
-	}
-	
-
+	model_->Draw(worldTransform_,viewProjection_[cameraNum],textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
