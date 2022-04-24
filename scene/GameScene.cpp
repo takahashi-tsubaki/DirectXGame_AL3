84aec47953fb511edbe097ff9aee17fc969af09e
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-25 07:17:21 +0900

    新規画像の追加及び課題の完成

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -16,22 +16,49 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 	//ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("mario(2).jpg");
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	reticleHandle_ = TextureManager::Load("reticle.png");
+
+	sprite_ = Sprite::Create(reticleHandle_, {576, 296});
 	//３Dモデルの生成
 	model_ = Model::Create();
 
+	//乱数シード生成器
+	std::random_device seed_gen;
+
+	//メルセンヌ・ツイスター
+	std::mt19937_64 engine(seed_gen());
+
+	//乱数範囲(回転角用)
+	std::uniform_real_distribution<float> rotDist(0.0f,XM_2PI);
+
+	//乱数範囲(座標用)
+	std::uniform_real_distribution<float> posDist(-10.0f,10.0f);
+
+
 	for (int i = 0;i<_countof(worldTransform_);i++) 
 	{
-		for (int j = 0; j < _countof(worldTransform_); j++) 
-		{
+		
+		//画像の大きさ
+		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
 
-			worldTransform_[j][i].translation_ = {-12.0f + (i * 4.0f), -12.0f + (j*4.0f),0.0f};
+		//X,Y,Zの回転
+		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
 
-			//ワールドトランスフォームの初期化
-			worldTransform_[j][i].Initialize();
-		}
+		//X,Y,Z軸の平行移動
+		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
+
+		//ワールドトランスフォームの初期化
+		worldTransform_[i].Initialize();
 		
 	}
+
+	viewProjection_.eye = {0,0,-30};
+
+	viewProjection_.fovAngleY = XMConvertToRadians(10.0f);
+
+	//ニアクリップ距離
+	viewProjection_.nearZ = 0.1f;
 
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
@@ -45,19 +72,19 @@ void GameScene::Update()
 
 	const float kTargetSpeed = 0.2f;
 
-	if (input_->PushKey(DIK_W)) 
+	if (input_->PushKey(DIK_UP)) 
 	{
 		move = {0, kTargetSpeed, 0};
 	}
-	else if(input_->PushKey(DIK_S)) 
+	else if(input_->PushKey(DIK_DOWN)) 
 	{
 		move = {0, -kTargetSpeed, 0};
 	}
-	if (input_->PushKey(DIK_D)) 
+	if (input_->PushKey(DIK_RIGHT)) 
 	{
 		move = {kTargetSpeed,0, 0};
 	} 
-	else if (input_->PushKey(DIK_A)) 
+	else if (input_->PushKey(DIK_LEFT)) 
 	{
 		move = {-kTargetSpeed,0, 0};
 	}
@@ -68,16 +95,27 @@ void GameScene::Update()
 
 	viewProjection_.UpdateMatrix();
 
-	if (input_->PushKey(DIK_UP)) 
+	if (input_->TriggerKey(DIK_SPACE))
 	{
-		viewProjection_.fovAngleY += 0.01f;
-		viewProjection_.fovAngleY = min(viewProjection_.fovAngleY, XM_PI);
+		if (scope == 0)
+		{
+			scope = 1;
+		} 
+		else 
+		{
+			scope = 0;
+		}
 	}
 
-	else if (input_->PushKey(DIK_DOWN)) {
-		viewProjection_.fovAngleY -= 0.01f;
-		viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.01f);
+	if (scope == 1)
+	{
+		viewProjection_.fovAngleY = 19.999998f;
 	}
+	else
+	{
+		viewProjection_.fovAngleY = 39.999996f;
+	}
+
 	viewProjection_.UpdateMatrix();
 
 	debugText_->SetPos(50, 50);
@@ -93,6 +131,11 @@ void GameScene::Update()
 	    "up:(%f,%f,%f)\n", viewProjection_.up.x, viewProjection_.up.y,
 	    viewProjection_.up.z);
 
+	debugText_->SetPos(50, 110);
+	debugText_->Printf("forAngleY(Degree):%f\n",viewProjection_.fovAngleY);
+
+	debugText_->SetPos(50, 130);
+	debugText_->Printf("nearZ:%f\n", viewProjection_.nearZ);
 		////行列の再計算
 		//viewProjection_[i].UpdateMatrix();
 }
@@ -127,10 +170,9 @@ void GameScene::Draw() {
 	// 3Dモデル描画
 	for (int i = 0; i < _countof(worldTransform_); i++) 
 	{
-		for (int j = 0; j < _countof(worldTransform_); j++) 
-		{
-			model_->Draw(worldTransform_[j][i], viewProjection_, textureHandle_);
-		}
+		
+		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+		
 	}
 	
 
@@ -146,6 +188,11 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+
+	if (scope == 1) 
+	{
+		sprite_->Draw();
+	}
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
